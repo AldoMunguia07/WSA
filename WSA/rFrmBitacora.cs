999568e486092dd7636692e1c29e10629849cf18
Aldo Munguía <aldojosuemh@gmail.com>
2022-12-15 12:01:10 -0600

    Mejoras en tamaño de las paginas de los reportes

--- WSA/rFrmBitacora.cs
+++ WSA/rFrmBitacora.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Printing;
 
 namespace WSA
 {
@@ -22,6 +23,14 @@ namespace WSA
         {
             // TODO: esta línea de código carga datos en la tabla 'AWS_DATA_SET.sp_Reporte_Bitacora' Puede moverla o quitarla según sea necesario.
             this.sp_Reporte_BitacoraTableAdapter.Fill(this.AWS_DATA_SET.sp_Reporte_Bitacora, txtBuscar.Text, dtpFechaInicio.Value.Date, dtpFechaFinal.Value.Date);
+            /*PageSettings ps = new PageSettings();
+            ps.Landscape = true;
+            ps.PaperSize = new System.Drawing.Printing.PaperSize("A4", 210, 297);
+           // ps.PaperSize.RawKind = (int)System.Drawing.Printing.PaperKind.A4;
+
+            reportViewer1.SetPageSettings(ps);*/
+            //this.reportViewer1.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout);
+            
             this.reportViewer1.RefreshReport();
 
         }
