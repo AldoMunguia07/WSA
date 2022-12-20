159f489dd2d922881432f6e29cec2543ac4ed719
Aldo Munguía <aldojosuemh@gmail.com>
2022-12-20 08:11:23 -0600

    Mejora en tamaño de formulario de inicio de sesión.

--- WSA/FrmLogin.Designer.cs
+++ WSA/FrmLogin.Designer.cs
@@ -44,7 +44,7 @@ namespace WSA
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(83, 318);
+            this.label3.Location = new System.Drawing.Point(42, 318);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(77, 18);
@@ -55,7 +55,7 @@ namespace WSA
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(56, 412);
+            this.label4.Location = new System.Drawing.Point(15, 412);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(108, 18);
@@ -65,7 +65,7 @@ namespace WSA
             // txtUsuario
             // 
             this.txtUsuario.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsuario.Location = new System.Drawing.Point(172, 314);
+            this.txtUsuario.Location = new System.Drawing.Point(131, 314);
             this.txtUsuario.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtUsuario.Name = "txtUsuario";
             this.txtUsuario.Size = new System.Drawing.Size(190, 27);
@@ -75,7 +75,7 @@ namespace WSA
             // txtContrasena
             // 
             this.txtContrasena.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContrasena.Location = new System.Drawing.Point(172, 408);
+            this.txtContrasena.Location = new System.Drawing.Point(131, 408);
             this.txtContrasena.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtContrasena.Name = "txtContrasena";
             this.txtContrasena.Size = new System.Drawing.Size(190, 27);
@@ -89,7 +89,7 @@ namespace WSA
             this.btnInicioSesion.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnInicioSesion.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInicioSesion.ForeColor = System.Drawing.Color.White;
-            this.btnInicioSesion.Location = new System.Drawing.Point(146, 502);
+            this.btnInicioSesion.Location = new System.Drawing.Point(105, 502);
             this.btnInicioSesion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnInicioSesion.Name = "btnInicioSesion";
             this.btnInicioSesion.Size = new System.Drawing.Size(150, 32);
@@ -102,7 +102,7 @@ namespace WSA
             // 
             this.chbVerContraseña.AutoSize = true;
             this.chbVerContraseña.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chbVerContraseña.Location = new System.Drawing.Point(172, 443);
+            this.chbVerContraseña.Location = new System.Drawing.Point(131, 443);
             this.chbVerContraseña.Name = "chbVerContraseña";
             this.chbVerContraseña.Size = new System.Drawing.Size(127, 20);
             this.chbVerContraseña.TabIndex = 2;
@@ -113,7 +113,7 @@ namespace WSA
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WSA.Properties.Resources.Logo_AWS;
-            this.pictureBox1.Location = new System.Drawing.Point(88, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(47, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(274, 331);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -126,7 +126,7 @@ namespace WSA
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(427, 615);
+            this.ClientSize = new System.Drawing.Size(378, 594);
             this.Controls.Add(this.chbVerContraseña);
             this.Controls.Add(this.btnInicioSesion);
             this.Controls.Add(this.txtContrasena);
